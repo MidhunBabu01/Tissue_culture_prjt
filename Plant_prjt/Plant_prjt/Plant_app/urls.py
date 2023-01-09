9fe5815bbea907bfba786e3452721bfc82d80899
MidhunBabu01 <midhunkb57@gmail.com>
2023-01-09 20:05:18 +0530

    about as page added

--- Plant_prjt/Plant_prjt/Plant_app/urls.py
+++ Plant_prjt/Plant_prjt/Plant_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from Plant_app import views
 
+
+app_name = 'Plant_app'
+
+
 urlpatterns = [
-    path('', views.index,name="index")
+    path('', views.index,name="index"),
+    path('about/', views.about,name="about")
+    
 ]
--- Plant_prjt/Plant_prjt/Plant_app/views.py
+++ Plant_prjt/Plant_prjt/Plant_app/views.py
@@ -4,3 +4,8 @@ from django.http.response import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+
+
+def about(request):
+    return render(request,'about.html')
