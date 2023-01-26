22c889ecefa96b7ea5b9b1277f43758e0f3b2a08
MidhunBabu01 <midhunkb57@gmail.com>
2023-01-26 19:03:20 +0530

    pip upgrades, product page added

--- Plant_prjt/Plant_prjt/Plant_app/urls.py
+++ Plant_prjt/Plant_prjt/Plant_app/urls.py
@@ -7,6 +7,8 @@ app_name = 'Plant_app'
 
 urlpatterns = [
     path('', views.index,name="index"),
-    path('about/', views.about,name="about")
+    path('about/', views.about,name="about"),
+    path('products/', views.products,name="products")
+    
     
 ]
--- Plant_prjt/Plant_prjt/Plant_app/views.py
+++ Plant_prjt/Plant_prjt/Plant_app/views.py
@@ -9,3 +9,7 @@ def index(request):
 
 def about(request):
     return render(request,'about.html')
+
+
+def products(request):
+    return render(request,'products.html')
