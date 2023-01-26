e6ef420d9573984ef980899254fa9d7fdd6bd180
MidhunBabu01 <midhunkb57@gmail.com>
2023-01-26 20:37:24 +0530

    products, category model addded

--- Plant_prjt/Plant_prjt/Plant_app/admin.py
+++ Plant_prjt/Plant_prjt/Plant_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ProductsCategory,Products
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(ProductsCategory,CategoryAdmin)
+
+
+class ProductsAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','category','photo1','photo2','photo3']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Products)
--- Plant_prjt/Plant_prjt/Plant_app/models.py
+++ Plant_prjt/Plant_prjt/Plant_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductsCategory(models.Model):
+    class Meta:
+        verbose_name_plural = 'Products Category'
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100,unique=True)
+
+
+class Products(models.Model):
+    class Meta:
+        verbose_name_plural = 'Products'
+    name = models.CharField(max_length=100,blank=False,null=False,verbose_name="Specious Name")
+    slug = models.SlugField(max_length=100,unique=True)
+    category = models.ForeignKey(ProductsCategory,on_delete=models.SET_NULL,null=True)
+    Photo1 = models.ImageField(upload_to='Products Photos',blank=True,null=True)
+    Photo2 = models.ImageField(upload_to='Products Photos',blank=True,null=True) 
+    Photo3 = models.ImageField(upload_to='Products Photos',blank=True,null=True)
+
